09a0d52be2381412aa3a25c268c99cd16e1d8cec
Al Fair <thealisonfair@gmail.com>
2018-01-24 15:56:28 -0500

    task received with confirmation sent to sender

--- app.rb
+++ app.rb
@@ -1,6 +1,10 @@
 require 'sinatra'
 require 'json'
 require 'pry'
+require 'dotenv'
+require 'httparty'
+
+Dotenv.load
 
 get '/' do
   'Hello world!'
@@ -8,30 +12,54 @@ end
 
 post '/tasky' do
   request_data = JSON.parse(request.body.read)
+  puts request_data
   case request_data['type']
     when 'url_verification'
       request_data['challenge']
 
     when 'event_callback'
-      puts "it's an event"
-      event_data = request_data["event"]
-      tasker = event_data["user"]
 
-      task_arr = event_data["text"].split
-      taskee = task_arr.shift.delete('<' '@' '>')
-      task = task_arr.join(' ')
-      puts "tasker: #{tasker}"
-      puts "taskee: #{taskee}"
-      puts "task: #{task}"
+      event_data = request_data["event"]
+      # binding.pry
+      if !event_data["bot_id"]
+        puts "it's an event"
+        tasker = event_data["user"]
+
+        task_arr = event_data["text"].split
+        taskee = task_arr.shift
+        task = task_arr.join(' ')
+
+        puts "tasker: #{tasker}"
+        puts "taskee: #{taskee}"
+        puts "task: #{task}"
+
+
+
+
+        response = JSON.parse(HTTParty.post("https://slack.com/api/im.open",
+          body: {
+            "token" => ENV['SLACK_API_TOKEN'],
+            "user" => "#{tasker}"
+          },
+          headers: { 'Content-Type' => 'application/x-www-form-urlencoded' }
+        ).to_s)
+
+        channel = response["channel"]["id"]
+
+
+        HTTParty.post("https://slack.com/api/chat.postMessage",
+           body:  {
+              "token" => ENV['SLACK_API_TOKEN'],
+              "text" => "#{task} assigned to #{taskee}",
+              "channel" => "#{channel}",
+              "username" => "Tasky",
+              "as_user" => "false"
+            },
+         :headers => { 'Content-Type' => 'application/x-www-form-urlencoded' }
+       )
+
+     end
+
 
   end
 end
-
-
-
-# {"type"=>"message",
-#  "user"=>"U8WKR3K2Q",
-#  "text"=>"<@U8WKR3K2Q> buy pizza",
-#  "ts"=>"1516743130.000013",
-#  "channel"=>"D8WNCB1UY",
-#  "event_ts"=>"1516743130.000013"}
